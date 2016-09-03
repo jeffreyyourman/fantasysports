68f2089950543ea9e44dbafaeac5fe1752162f74
jeffreyyourman <jeffreyyourman@gmail.com>
2016-09-03 00:31:30 -0400

    fixed database

--- db/convertcsv.sql
+++ db/convertcsv.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE fantasy;
 use fantasy
 
-CREATE TABLE data(
+CREATE TABLE NFLDataQB(
   Week INT,
   Position VARCHAR(255) CHARACTER SET utf8,
   Team VARCHAR(255) CHARACTER SET utf8,
@@ -19,7 +19,93 @@ CREATE TABLE data(
   Criteria_Score INT,
   FP INT
 );
-INSERT INTO data(Week,Position,Team,Team_Name,Player_Name,Injury_Status,Type_of_Injury,Games_Played,Spread,Player_Salary,Rank_Vs_Pos,Player_Rank,OU,Rating,Criteria_Score,FP) VALUES (11,'WR','ATL','Atlanta','Julio Jones',NULL,NULL,9,-5,9100,25,11,47.5,9.986533687,7.139129826,20.5);
-INSERT INTO data(Week,Position,Team,Team_Name,Player_Name,Injury_Status,Type_of_Injury,Games_Played,Spread,Player_Salary,Rank_Vs_Pos,Player_Rank,OU,Rating,Criteria_Score,FP) VALUES (11,'WR','HOU','Houston','DeAndre Hopkins','P','Knee',9,3.5,8900,18,30,40,8.319055398,6.634980539,26.3);
-INSERT INTO data(Week,Position,Team,Team_Name,Player_Name,Injury_Status,Type_of_Injury,Games_Played,Spread,Player_Salary,Rank_Vs_Pos,Player_Rank,OU,Rating,Criteria_Score,FP) VALUES (11,'WR','DAL','Dallas','Dez Bryant','Q','Knee',4,-2,8400,20,23,46,8.534897841,1.476095004,12.5);
-INSERT INTO data(Week,Position,Team,Team_Name,Player_Name,Injury_Status,Type_of_Injury,Games_Played,Spread,Player_Salary,Rank_Vs_Pos,Player_Rank,OU,Rating,Criteria_Score,FP) VALUES (11,'WR','CIN','Cincinnati','A.J. Green',NULL,NULL,9,5,8200,11,7,48,10.2724627,9.350960561,9.9);
+CREATE TABLE NFLDataRB(
+  Week INT,
+  Position VARCHAR(255) CHARACTER SET utf8,
+  Team VARCHAR(255) CHARACTER SET utf8,
+  Team_Name VARCHAR(255) CHARACTER SET utf8,
+  Player_Name VARCHAR(255) CHARACTER SET utf8,
+  Injury_Status VARCHAR(255) CHARACTER SET utf8,
+  Type_of_Injury VARCHAR(255) CHARACTER SET utf8,
+  Games_Played INT,
+  Spread INT,
+  Player_Salary INT,
+  Rank_Vs_Pos INT,
+  Player_Rank INT,
+  OU INT,
+  Rating INT,
+  Criteria_Score INT,
+  FP INT
+);
+CREATE TABLE NFLDataWR(
+  Week INT,
+  Position VARCHAR(255) CHARACTER SET utf8,
+  Team VARCHAR(255) CHARACTER SET utf8,
+  Team_Name VARCHAR(255) CHARACTER SET utf8,
+  Player_Name VARCHAR(255) CHARACTER SET utf8,
+  Injury_Status VARCHAR(255) CHARACTER SET utf8,
+  Type_of_Injury VARCHAR(255) CHARACTER SET utf8,
+  Games_Played INT,
+  Spread INT,
+  Player_Salary INT,
+  Rank_Vs_Pos INT,
+  Player_Rank INT,
+  OU INT,
+  Rating INT,
+  Criteria_Score INT,
+  FP INT
+);
+CREATE TABLE NFLDataTE(
+  Week INT,
+  Position VARCHAR(255) CHARACTER SET utf8,
+  Team VARCHAR(255) CHARACTER SET utf8,
+  Team_Name VARCHAR(255) CHARACTER SET utf8,
+  Player_Name VARCHAR(255) CHARACTER SET utf8,
+  Injury_Status VARCHAR(255) CHARACTER SET utf8,
+  Type_of_Injury VARCHAR(255) CHARACTER SET utf8,
+  Games_Played INT,
+  Spread INT,
+  Player_Salary INT,
+  Rank_Vs_Pos INT,
+  Player_Rank INT,
+  OU INT,
+  Rating INT,
+  Criteria_Score INT,
+  FP INT
+);
+CREATE TABLE NFLDataK(
+  Week INT,
+  Position VARCHAR(255) CHARACTER SET utf8,
+  Team VARCHAR(255) CHARACTER SET utf8,
+  Team_Name VARCHAR(255) CHARACTER SET utf8,
+  Player_Name VARCHAR(255) CHARACTER SET utf8,
+  Injury_Status VARCHAR(255) CHARACTER SET utf8,
+  Type_of_Injury VARCHAR(255) CHARACTER SET utf8,
+  Games_Played INT,
+  Spread INT,
+  Player_Salary INT,
+  Rank_Vs_Pos INT,
+  Player_Rank INT,
+  OU INT,
+  Rating INT,
+  Criteria_Score INT,
+  FP INT
+);
+CREATE TABLE NFLDataDef(
+  Week INT,
+  Position VARCHAR(255) CHARACTER SET utf8,
+  Team VARCHAR(255) CHARACTER SET utf8,
+  Team_Name VARCHAR(255) CHARACTER SET utf8,
+  Player_Name VARCHAR(255) CHARACTER SET utf8,
+  Injury_Status VARCHAR(255) CHARACTER SET utf8,
+  Type_of_Injury VARCHAR(255) CHARACTER SET utf8,
+  Games_Played INT,
+  Spread INT,
+  Player_Salary INT,
+  Rank_Vs_Pos INT,
+  Player_Rank INT,
+  OU INT,
+  Rating INT,
+  Criteria_Score INT,
+  FP INT
+);
--- model/fantasydata.js
+++ model/fantasydata.js
@@ -1,36 +1,166 @@
 /*
-Here is where you setup a model for how to interface with the database.
+Here is where you setup a model for how to interface with the NFLDataQBbase.
 */
 
 var orm = require('../config/orm.js');
 
-var user = {
-	findOneFantasyData: function(condition, cb) {
-	  orm.findOneFantasyData('data', condition, function(res){
+var NFLData = {
+	findOneFantasyDataQB: function(condition, cb) {
+	  orm.findOneFantasyDataQB('NFLDataQB', condition, function(res){
 	      cb(res);
 	  });
   },
-	allFantasyData: function(cb) {
-		orm.allFantasyData('data', function(res){
+	allFantasyDataQB: function(cb) {
+		orm.allFantasyDataQB('NFLDataQB', function(res){
 			cb(res);
 		});
 	},
 	//cols and vals are arrays
-	createFantasyData: function(cols, vals, cb) {
-		orm.createFantasyData('data', cols, vals, function(res){
+	createFantasyDataQB: function(cols, vals, cb) {
+		orm.createFantasyDataQB('NFLDataQB', cols, vals, function(res){
 			cb(res);
 		});
 	},
-	updateFantasyData: function(objColVals, condition, cb) {
-		orm.updateFantasyData('data', objColVals, condition, function(res){
+	updateFantasyDataQB: function(objColVals, condition, cb) {
+		orm.updateFantasyDataQB('NFLDataQB', objColVals, condition, function(res){
 			cb(res);
 		});
 	},
-	deleteFantasyData: function(condition, cb){
-		orm.deleteFantasyData('data', condition, function(res){
+	deleteFantasyDataQB: function(condition, cb){
+		orm.deleteFantasyDataQB('NFLDataQB', condition, function(res){
+			cb(res);
+		});
+	},
+	findOneFantasyDataRB: function(condition, cb) {
+		orm.findOneFantasyDataRB('NFLDataRB', condition, function(res){
+				cb(res);
+		});
+	},
+	allFantasyDataRB: function(cb) {
+		orm.allFantasyDataRB('NFLDataRB', function(res){
+			cb(res);
+		});
+	},
+	//cols and vals are arrays
+	createFantasyDataRB: function(cols, vals, cb) {
+		orm.createFantasyDataRB('NFLDataRB', cols, vals, function(res){
+			cb(res);
+		});
+	},
+	updateFantasyDataRB: function(objColVals, condition, cb) {
+		orm.updateFantasyDataRB('NFLDataRB', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	deleteFantasyDataRB: function(condition, cb){
+		orm.deleteFantasyDataRB('NFLDataRB', condition, function(res){
+			cb(res);
+		});
+	},
+	findOneFantasyDataWR: function(condition, cb) {
+		orm.findOneFantasyDataWR('NFLDataWR', condition, function(res){
+				cb(res);
+		});
+	},
+	allFantasyDataWR: function(cb) {
+		orm.allFantasyDataWR('NFLDataWR', function(res){
+			cb(res);
+		});
+	},
+	//cols and vals are arrays
+	createFantasyDataWR: function(cols, vals, cb) {
+		orm.createFantasyDataWR('NFLDataWR', cols, vals, function(res){
+			cb(res);
+		});
+	},
+	updateFantasyDataWR: function(objColVals, condition, cb) {
+		orm.updateFantasyDataWR('NFLDataWR', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	deleteFantasyDataWR: function(condition, cb){
+		orm.deleteFantasyDataWR('NFLDataWR', condition, function(res){
+			cb(res);
+		});
+	},
+	findOneFantasyDataTE: function(condition, cb) {
+		orm.findOneFantasyDataTE('NFLDataTE', condition, function(res){
+				cb(res);
+		});
+	},
+	allFantasyDataTE: function(cb) {
+		orm.allFantasyDataTE('NFLDataTE', function(res){
+			cb(res);
+		});
+	},
+	//cols and vals are arrays
+	createFantasyDataTE: function(cols, vals, cb) {
+		orm.createFantasyDataTE('NFLDataTE', cols, vals, function(res){
+			cb(res);
+		});
+	},
+	updateFantasyDataTE: function(objColVals, condition, cb) {
+		orm.updateFantasyDataTE('NFLDataTE', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	deleteFantasyDataTE: function(condition, cb){
+		orm.deleteFantasyDataTE('NFLDataTE', condition, function(res){
+			cb(res);
+		});
+	},
+	findOneFantasyDataK: function(condition, cb) {
+		orm.findOneFantasyDataK('NFLDataK', condition, function(res){
+				cb(res);
+		});
+	},
+	allFantasyDataK: function(cb) {
+		orm.allFantasyDataK('NFLDataK', function(res){
+			cb(res);
+		});
+	},
+	//cols and vals are arrays
+	createFantasyDataK: function(cols, vals, cb) {
+		orm.createFantasyDataK('NFLDataK', cols, vals, function(res){
+			cb(res);
+		});
+	},
+	updateFantasyDataK: function(objColVals, condition, cb) {
+		orm.updateFantasyDataK('NFLDataK', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	deleteFantasyDataK: function(condition, cb){
+		orm.deleteFantasyDataK('NFLDataK', condition, function(res){
+			cb(res);
+		});
+	},
+	findOneFantasyDataDef: function(condition, cb) {
+		orm.findOneFantasyDataDef('NFLDataDef', condition, function(res){
+				cb(res);
+		});
+	},
+	allFantasyDataDef: function(cb) {
+		orm.allFantasyDataDef('NFLDataDef', function(res){
+			cb(res);
+		});
+	},
+	//cols and vals are arrays
+	createFantasyDataDef: function(cols, vals, cb) {
+		orm.createFantasyDataDef('NFLDataDef', cols, vals, function(res){
+			cb(res);
+		});
+	},
+	updateFantasyDataDef: function(objColVals, condition, cb) {
+		orm.updateFantasyDataDef('NFLDataDef', objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+	deleteFantasyDataDef: function(condition, cb){
+		orm.deleteFantasyDataDef('NFLDataDef', condition, function(res){
 			cb(res);
 		});
 	}
 };
 
-module.exports = user;
+module.exports = NFLData;
