12f4f6dfcf02b3b104086dc5e404787f5526e187
jeffreyyourman <jeffreyyourman@gmail.com>
2016-09-05 01:05:07 -0400

    fdsafdsa

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,13 @@ if (process.env.JAWSDB_URL){
 } else {
     connection = mysql.createConnection({
         host: "uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: 'yk1rjm4o00htkeuq',
-        password: 'om9h5zgmk9nx6gps',
-        database: 'sroybkfc87fbqn7g',
+        user: 'bx2ajv69tsflz7fv',
+        password: 'bk7cnbekaw907zll',
+        database: 'wl8zg20cpj22w77k',
     });
 }
-// mysql -h uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com -u yk1rjm4o00htkeuq -pom9h5zgmk9nx6gps sroybkfc87fbqn7g < backup.sql
+// "mysql://bx2ajv69tsflz7fv:bk7cnbekaw907zll@uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wl8zg20cpj22w77k"
+// mysql -h uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com -u bx2ajv69tsflz7fv -pbk7cnbekaw907zll wl8zg20cpj22w77k < backup.sql
 
 // mysqldump -h localhost -u root -p fantasy > backup.sql
 // var connection = mysql.createConnection({
@@ -31,3 +32,5 @@ connection.connect(function(err){
 });
 
 module.exports = connection;
+
+// heroku restart -a wearefantasy
