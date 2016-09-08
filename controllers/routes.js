79464b77e2d83b95ca3458d2ac0f60acbef222ee
jeffreyyourman <jeffreyyourman@gmail.com>
2016-09-08 14:36:11 -0400

    added cheerio

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 //models below
 var user = require('../model/userdata.js');
 var fantasydata = require('../model/fantasydata.js');
@@ -7,6 +9,29 @@ var fantasydata = require('../model/fantasydata.js');
 var connection = require('../config/connection.js');
 
 router.get('/', function(req, res){
+
+    request('http://www.rotoworld.com/playernews/nfl/football/', function (error, response, html) {
+    // console.log('my name is tim',html);
+      var $ = cheerio.load(html);
+      var result = [];
+      $('div#cp1_ctl00_rptBlurbs_floatingcontainer_0').each(function(i, element){
+
+          var title = $(element).children('.report').text()
+          var description = $(element).children('.impact').text()
+          var info = $(element).children('.info').text()
+            //
+            // console.log(title);
+            // console.log(description);
+            // console.log(info);
+          result.push({
+            title:title,
+            description:description,
+            info:info
+          });
+        });
+      console.log(result);
+    });
+
   var email = req.session.user_email;
 
   var condition = "email = '" + email + "'";
