d340deccbd03f7feacc73f9b67d16c13d16ec8c7
jeffreyyourman <jeffreyyourman@gmail.com>
2016-10-11 13:13:44 -0400

    google

--- controllers/fantasydata_controller.js
+++ controllers/fantasydata_controller.js
@@ -14,44 +14,4 @@ router.get('/NFL', function(req,res){
       }
       res.render('fantasydata/fantasy_data', hbsObject);
 });
-
-router.get('/NFL/QB', function(req,res){
-      var hbsObject = {
-        logged_in:req.session.logged_in
-      }
-      res.render('fantasydata/fantasy_data', hbsObject);
-
-});
-
-router.get('/NFL/RB', function(req,res){
-      var hbsObject = {
-        logged_in: req.session.logged_in
-      }
-      res.render('fantasydata/fantasy_data', hbsObject);
-});
-router.get('/NFL/WR', function(req,res){
-      var hbsObject = {
-        logged_in: req.session.logged_in
-      }
-      res.render('fantasydata/fantasy_data', hbsObject);
-});
-router.get('/NFL/TE', function(req,res){
-      var hbsObject = {
-        logged_in: req.session.logged_in
-      }
-      res.render('fantasydata/fantasy_data', hbsObject);
-});
-router.get('/NFL/K', function(req,res){
-      var hbsObject = {
-        logged_in: req.session.logged_in
-      }
-      res.render('fantasydata/fantasy_data', hbsObject);
-});
-router.get('/NFL/Def', function(req,res){
-      var hbsObject = {
-        logged_in: req.session.logged_in
-      }
-      res.render('fantasydata/fantasy_data', hbsObject);
-});
-
 module.exports = router;
