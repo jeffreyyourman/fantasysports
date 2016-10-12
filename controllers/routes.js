ea78e5568fcb10555a4a2c1aaf811d2abd1df900
jeffreyyourman <jeffreyyourman@gmail.com>
2016-10-11 20:10:25 -0400

    cleaned up modules getting ready to new firebase chat

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,10 +3,7 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 var Twitter = require('twitter');
-// var dateFormat = require('dateformat');
-// var now = new Date();
-// date: dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT")
-//models below
+
 var user = require('../model/userdata.js');
 var fantasydata = require('../model/fantasydata.js');
 var fantasynews = require('../model/fantasynews.js');
@@ -20,57 +17,40 @@ router.get('/', function(req, res){
   var urlNBA = "http://www.cbssports.com/fantasy/basketball/players/news/all/";
   var url2NBA = "http://www.cbssports.com/fantasy/basketball/players/news/all/2/";
 
-  request(url2NBA, function (error, response, html) {
-    var $ = cheerio.load(html);
-    var name,time,title,injuryreport;
-    // var json = {playernameandteam: '', time: '', playerreport: ''};
-    var json = {playernameandteam: ''};
-  $('div.player-news-desc').filter(function(i, element){
-      var playernameandteam = $(element).children('h4').text();
-      fantasynews.createNewsNBA(['fantasynews'], [playernameandteam], function(fantasynewsnba){
-        //can log here if i like
-      })
+  function cheerioNBA(url) {
+    request(url, function (error, response, html) {
+      var $ = cheerio.load(html);
+      var name,time,title,injuryreport;
+      // var json = {playernameandteam: '', time: '', playerreport: ''};
+      var json = {playernameandteam: ''};
+    $('div.player-news-desc').filter(function(i, element){
+        var playernameandteam = $(element).children('h4').text();
+        fantasynews.createNewsNBA(['fantasynews'], [playernameandteam], function(fantasynewsnba){
+          //can log here if i like
+        })
+      });
     });
-  });
+  }
 
-  request(urlNBA, function (error, response, html) {
-    var $ = cheerio.load(html);
-    var name,time,title,injuryreport;
-    // var json = {playernameandteam: '', time: '', playerreport: ''};
-    var json = {playernameandteam: ''};
-  $('div.player-news-desc').filter(function(i, element){
-      var playernameandteam = $(element).children('h4').text();
-      fantasynews.createNewsNBA(['fantasynews'], [playernameandteam], function(fantasynewsnba){
-        //can log here if i like
-      })
+  function cheerioNFL(url) {
+    request(url, function (error, response, html) {
+      var $ = cheerio.load(html);
+      var name,time,title,injuryreport;
+      // var json = {playernameandteam: '', time: '', playerreport: ''};
+      var json = {playernameandteam: ''};
+    $('div.player-news-desc').filter(function(i, element){
+        var playernameandteam = $(element).children('h4').text();
+        fantasynews.createNews(['fantasynews'], [playernameandteam], function(fantasynewsnba){
+          //can log here if i like
+        })
+      });
     });
-  });
+  }
+  cheerioNBA(urlNBA);
+  cheerioNBA(url2NBA);
 
-  request(url2NFL, function (error, response, html) {
-    var $ = cheerio.load(html);
-    var name,time,title,injuryreport;
-    // var json = {playernameandteam: '', time: '', playerreport: ''};
-    var json = {playernameandteam: ''};
-  $('div.player-news-desc').filter(function(i, element){
-      var playernameandteam = $(element).children('h4').text();
-      fantasynews.createNews(['fantasynews'], [playernameandteam], function(fantasynews){
-        //can log here if i like
-      })
-    });
-  });
-
-  request(urlNFL, function (error, response, html) {
-    var $ = cheerio.load(html);
-    var name,time,title,injuryreport;
-    // var json = {playernameandteam: '', time: '', playerreport: ''};
-    var json = {playernameandteam: ''};
-  $('div.player-news-desc').filter(function(i, element){
-      var playernameandteam = $(element).children('h4').text();
-      fantasynews.createNews(['fantasynews'], [playernameandteam], function(fantasynews){
-        //can log here if i like
-      })
-    });
-  });
+  cheerioNFL(urlNFL);
+  cheerioNFL(url2NFL);
 
   var email = req.session.user_email;
   var condition = "email = '" + email + "'";
@@ -98,3 +78,10 @@ router.get('/Chat', function(req,res){
   res.render('users/chat')
 });
 module.exports = router;
+
+
+
+// var dateFormat = require('dateformat');
+// var now = new Date();
+// date: dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT")
+//models below
