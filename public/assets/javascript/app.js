5f6f270c67f2464bb29ece6fd94510502a245fe0
jeffreyyourman <jeffreyyourman@gmail.com>
2016-10-25 18:54:06 -0400

    started color coding process on fantasyqb table

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  // var table = $('.fantasyQB').DataTable();
+
+
 
   var spreadsheetIDqb = "1VTZtc8vRucqAHVO-7ACCMe1sm3vl5NC9DLtDuh622Jw";
   var urlqb = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDqb + "/od6/public/values?alt=json";
@@ -7,16 +10,9 @@ $(document).ready(function(){
 
       // if emptdy show all the appointments
       $.get(urlqb , function( data ) {
-      // $.get('/tables' , function( data ) {
-        // console.log('this si data', data);
-      // $.get('/tables', function( data ) {
-        // for (var i = 0; i < data.length; i++) {
-        //   console.log(data[i]);
-        // } this doesn't work (nothing logs)
-
-        // console.log(data); // this log works but comes up empty
         //display all appointments with DATA
-        $('.fantasyQB').DataTable( {
+        $('.fantasyQB').DataTable({
+
           data: data.feed.entry,
 
           "dom": '<"top"i>rt<"bottom"flp><"clear">',
@@ -25,6 +21,14 @@ $(document).ready(function(){
           "ordering": true,
           "info":     false,
           "bLengthChange": false,
+
+          // var table = $('#example').DataTable();
+          //
+          // table.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
+          //     var cell = table.cell({ row: rowIdx, column: 0 }).node();
+          //     $(cell).addClass('warning');
+          // });
+
 
           columns: [
         { data: 'gsx$week.$t' },
@@ -47,221 +51,247 @@ $(document).ready(function(){
         { data: 'gsx$salchange.$t' },
         { data: 'gsx$olrank.$t' },
         { data: 'gsx$fps.$t' }
-            ]
+      ],
+            "columnDefs": [ {
+              "targets": 7,
+              "createdCell": function (td, cellData, rowData, row, col) {
+                if ( cellData >= 20 ) {
+                  $(td).css('background-color', 'lightgreen')
+                } else if ((cellData >= 10) && (cellData <= 19)) {
+                  $(td).css('background-color', 'orange')
+                } else if ((cellData) >= 0 && (cellData <= 9)) {
+                  $(td).css('background-color', 'red')
+                }
+              }
+            } ]
           });
+          // $('.fantasyQB').DataTable({
+          //   "createdRow": function ( row, data, index ) {
+          //     if ( data[7].replace(/[\$,]/g, '') * 1 > 15 ) {
+          //         $('td', row).eq(7).addClass('highlight');
+          //     }
+          //   }
+          // });
         });
 
-
-    var spreadsheetIDrb = "15mSJN1WKrxFVJtIYT20sJS9mGr7f65Ow1XGLhHVuzDo";
-    // Make sure it is public or set to Anyone with link can view
-    var urlrb = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDrb + "/od6/public/values?alt=json";
-
-      // if empty show all the appointments
-      $.get(urlrb , function( data ) {
-
-        //display all appointments with DATA
-        $('.fantasyRB').DataTable( {
-
-          data: data.feed.entry,
-
-          "dom": '<"top"i>rt<"bottom"flp><"clear">',
-
-
-          "ordering": true,
-          "info":     false,
-          "bLengthChange": false,
-
-          columns: [
-            {data: "gsx$week.$t"},
-            {data: "gsx$player.$t"},
-            {data: "gsx$sal.$t"},
-            {data: "gsx$pos.$t"},
-            {data: "gsx$teamname.$t"},
-            {data: "gsx$injurystatus.$t"},
-            {data: "gsx$typeinjury.$t"},
-            {data: "gsx$spread.$t"},
-            {data: "gsx$rvp.$t"},
-            {data: "gsx$prating.$t"},
-            {data: "gsx$ppg.$t"},
-            {data: "gsx$rushattgame.$t"},
-            {data: "gsx$rectarg.$t"},
-            {data: "gsx$totalopp.$t"},
-            {data: "gsx$ppa.$t"},
-            {data: "gsx$expectedwins.$t"},
-            {data: "gsx$pathexpectation.$t"},
-            {data: "gsx$ou.$t"},
-            {data: "gsx$salchange.$t"},
-            {data: "gsx$snapcount.$t"},
-            {data: "gsx$oflrating.$t"},
-            {data: "gsx$fps.$t"}
-            ]
-          });
-        });
-
-  var spreadsheetIDwr = "1gNVt-L_tVtvNSvoZpmocD91q7JShDjC370JqXo4E6Ko";
-  var urlwr = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDwr + "/od6/public/values?alt=json";
-
-    // if empty show all the appointments
-    $.get(urlwr , function( data ) {
-
-      $('.fantasyWR').DataTable( {
-
-        data: data.feed.entry,
-
-        "dom": '<"top"i>rt<"bottom"flp><"clear">',
-
-
-        "ordering": true,
-        "info":     false,
-        "bLengthChange": false,
-
-        columns: [
-          {data: "gsx$week.$t"},
-          {data: "gsx$player.$t"},
-          {data: "gsx$sal.$t"},
-          {data: "gsx$pos.$t"},
-          {data: "gsx$teamname.$t"},
-          {data: "gsx$injurystatus.$t"},
-          {data: "gsx$typeinjury.$t"},
-          {data: "gsx$spread.$t"},
-          {data: "gsx$rvp.$t"},
-          {data: "gsx$pr.$t"},
-          {data: "gsx$ppg.$t"},
-          {data: "gsx$rushattgame.$t"},
-          {data: "gsx$rectarg.$t"},
-          {data: "gsx$recs.$t"},
-          {data: "gsx$totalopp.$t"},
-          {data: "gsx$ppa.$t"},
-          {data: "gsx$expectedwins.$t"},
-          {data: "gsx$ou.$t"},
-          {data: "gsx$salchange.$t"},
-          {data: "gsx$snapcount.$t"},
-          {data: "gsx$fps.$t"}
-          ]
-        });
-      });
-
-  var spreadsheetIDte = "1UjVGzANuKJ-_ERg8f4ixjPW6AVL9bjCcIhyq4YK-Z0k";
-  var urlte = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDte + "/od6/public/values?alt=json";
-
-    // if empty show all the appointments
-    $.get(urlte , function( data ) {
-
-      //display all appointments with DATA
-      $('.fantasyTE').DataTable( {
-
-        data: data.feed.entry,
-
-        "dom": '<"top"i>rt<"bottom"flp><"clear">',
-
-
-        "ordering": true,
-        "info":     false,
-        "bLengthChange": false,
-
-        columns: [
-          {data: "gsx$week.$t"},
-          {data: "gsx$player.$t"},
-          {data: "gsx$sal.$t"},
-          {data: "gsx$pos.$t"},
-          {data: "gsx$teamname.$t"},
-          {data: "gsx$injurystatus.$t"},
-          {data: "gsx$typeinjury.$t"},
-          {data: "gsx$spread.$t"},
-          {data: "gsx$qbscore.$t"},
-          {data: "gsx$rvp.$t"},
-          {data: "gsx$pr.$t"},
-          {data: "gsx$ppg.$t"},
-          {data: "gsx$rushattgame.$t"},
-          {data: "gsx$rectarg.$t"},
-          {data: "gsx$recs.$t"},
-          {data: "gsx$totalopp.$t"},
-          {data: "gsx$ppa.$t"},
-          {data: "gsx$expectedwins.$t"},
-          {data: "gsx$ou.$t"},
-          {data: "gsx$salchange.$t"},
-          {data: "gsx$snapcount.$t"},
-          {data: "gsx$fps.$t"}
-          ]
-        });
-      });
-
-  var spreadsheetIDk = "1RWrzWmrAZqyLlmpqNnA8kKRk9fjFN3japYiAtztRWKs";
-  var urlk = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDk + "/od6/public/values?alt=json";
-
-    $.get(urlk , function( data ) {
-
-      //display all appointments with DATA
-      $('.fantasyK').DataTable( {
-
-        data: data.feed.entry,
-
-        "dom": '<"top"i>rt<"bottom"flp><"clear">',
-
-
-        "ordering": true,
-        "info":     false,
-        "bLengthChange": false,
-
-        columns: [
-          {data: "gsx$week.$t"},
-          {data: "gsx$player.$t"},
-          {data: "gsx$sal.$t"},
-          {data: "gsx$pos.$t"},
-          {data: "gsx$teamname.$t"},
-          {data: "gsx$injurystatus.$t"},
-          {data: "gsx$typeinjury.$t"},
-          {data: "gsx$rvp.$t"},
-          {data: "gsx$pr.$t"},
-          {data: "gsx$ppg.$t"},
-          {data: "gsx$totalopp.$t"},
-          {data: "gsx$ppk.$t"},
-          {data: "gsx$expectedwins.$t"},
-          {data: "gsx$pathexpectation.$t"},
-          {data: "gsx$ou.$t"},
-          {data: "gsx$salchange.$t"},
-          {data: "gsx$fps.$t"}
-          ]
-        });
-      });
-
-  var spreadsheetIDdef = "1B63c8kUsBMC3nHhIq2vErR-55b-SgiTlhGSYIA-G2X4";
-
-  var urldef = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDdef + "/od6/public/values?alt=json";
-  $.get(urldef , function( data ) {
-
-    //display all appointments with DATA
-    $('.fantasyDEF').DataTable( {
-
-      data: data.feed.entry,
-
-      "dom": '<"top"i>rt<"bottom"flp><"clear">',
-
-
-      "ordering": true,
-      "info":     false,
-      "bLengthChange": false,
-
-      columns: [
-        {data: "gsx$week.$t"},
-        {data: "gsx$player.$t"},
-        {data: "gsx$sal.$t"},
-        {data: "gsx$pos.$t"},
-        {data: "gsx$teamname.$t"},
-        {data: "gsx$opp.$t"},
-        {data: "gsx$oppqbinjurystatus.$t"},
-        {data: "gsx$typeinjury.$t"},
-        {data: "gsx$opp-qbscore.$t"},
-        {data: "gsx$rvp.$t"},
-        {data: "gsx$pr.$t"},
-        {data: "gsx$yardsallowed.$t"},
-        {data: "gsx$expectedwins.$t"},
-        {data: "gsx$ou.$t"},
-        {data: "gsx$salchange.$t"},
-        {data: "gsx$fps.$t"}
-        ]
-      });
-    });
+        // $('.fantasyQB').DataTable({
+        //   "createdRow": function ( row, data, index ) {
+        //     if ( data[7].replace(/[\$,]/g, '') * 1 > 15 ) {
+        //         $('td', row).eq(7).addClass('highlight');
+        //     }
+        //   }
+        // });
+        // $(".fantasyQB tr td:nth-child(2)").css("background-color", "yellow");
+  //   var spreadsheetIDrb = "15mSJN1WKrxFVJtIYT20sJS9mGr7f65Ow1XGLhHVuzDo";
+  //   // Make sure it is public or set to Anyone with link can view
+  //   var urlrb = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDrb + "/od6/public/values?alt=json";
+  //
+  //     // if empty show all the appointments
+  //     $.get(urlrb , function( data ) {
+  //
+  //       //display all appointments with DATA
+  //       $('.fantasyRB').DataTable( {
+  //
+  //         data: data.feed.entry,
+  //
+  //         "dom": '<"top"i>rt<"bottom"flp><"clear">',
+  //
+  //
+  //         "ordering": true,
+  //         "info":     false,
+  //         "bLengthChange": false,
+  //
+  //         columns: [
+  //           {data: "gsx$week.$t"},
+  //           {data: "gsx$player.$t"},
+  //           {data: "gsx$sal.$t"},
+  //           {data: "gsx$pos.$t"},
+  //           {data: "gsx$teamname.$t"},
+  //           {data: "gsx$injurystatus.$t"},
+  //           {data: "gsx$typeinjury.$t"},
+  //           {data: "gsx$spread.$t"},
+  //           {data: "gsx$rvp.$t"},
+  //           {data: "gsx$prating.$t"},
+  //           {data: "gsx$ppg.$t"},
+  //           {data: "gsx$rushattgame.$t"},
+  //           {data: "gsx$rectarg.$t"},
+  //           {data: "gsx$totalopp.$t"},
+  //           {data: "gsx$ppa.$t"},
+  //           {data: "gsx$expectedwins.$t"},
+  //           {data: "gsx$pathexpectation.$t"},
+  //           {data: "gsx$ou.$t"},
+  //           {data: "gsx$salchange.$t"},
+  //           {data: "gsx$snapcount.$t"},
+  //           {data: "gsx$oflrating.$t"},
+  //           {data: "gsx$fps.$t"}
+  //           ]
+  //         });
+  //       });
+  //
+  // var spreadsheetIDwr = "1gNVt-L_tVtvNSvoZpmocD91q7JShDjC370JqXo4E6Ko";
+  // var urlwr = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDwr + "/od6/public/values?alt=json";
+  //
+  //   // if empty show all the appointments
+  //   $.get(urlwr , function( data ) {
+  //
+  //     $('.fantasyWR').DataTable( {
+  //
+  //       data: data.feed.entry,
+  //
+  //       "dom": '<"top"i>rt<"bottom"flp><"clear">',
+  //
+  //
+  //       "ordering": true,
+  //       "info":     false,
+  //       "bLengthChange": false,
+  //
+  //       columns: [
+  //         {data: "gsx$week.$t"},
+  //         {data: "gsx$player.$t"},
+  //         {data: "gsx$sal.$t"},
+  //         {data: "gsx$pos.$t"},
+  //         {data: "gsx$teamname.$t"},
+  //         {data: "gsx$injurystatus.$t"},
+  //         {data: "gsx$typeinjury.$t"},
+  //         {data: "gsx$spread.$t"},
+  //         {data: "gsx$rvp.$t"},
+  //         {data: "gsx$pr.$t"},
+  //         {data: "gsx$ppg.$t"},
+  //         {data: "gsx$rushattgame.$t"},
+  //         {data: "gsx$rectarg.$t"},
+  //         {data: "gsx$recs.$t"},
+  //         {data: "gsx$totalopp.$t"},
+  //         {data: "gsx$ppa.$t"},
+  //         {data: "gsx$expectedwins.$t"},
+  //         {data: "gsx$ou.$t"},
+  //         {data: "gsx$salchange.$t"},
+  //         {data: "gsx$snapcount.$t"},
+  //         {data: "gsx$fps.$t"}
+  //         ]
+  //       });
+  //     });
+  //
+  // var spreadsheetIDte = "1UjVGzANuKJ-_ERg8f4ixjPW6AVL9bjCcIhyq4YK-Z0k";
+  // var urlte = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDte + "/od6/public/values?alt=json";
+  //
+  //   // if empty show all the appointments
+  //   $.get(urlte , function( data ) {
+  //
+  //     //display all appointments with DATA
+  //     $('.fantasyTE').DataTable( {
+  //
+  //       data: data.feed.entry,
+  //
+  //       "dom": '<"top"i>rt<"bottom"flp><"clear">',
+  //
+  //
+  //       "ordering": true,
+  //       "info":     false,
+  //       "bLengthChange": false,
+  //
+  //       columns: [
+  //         {data: "gsx$week.$t"},
+  //         {data: "gsx$player.$t"},
+  //         {data: "gsx$sal.$t"},
+  //         {data: "gsx$pos.$t"},
+  //         {data: "gsx$teamname.$t"},
+  //         {data: "gsx$injurystatus.$t"},
+  //         {data: "gsx$typeinjury.$t"},
+  //         {data: "gsx$spread.$t"},
+  //         {data: "gsx$qbscore.$t"},
+  //         {data: "gsx$rvp.$t"},
+  //         {data: "gsx$pr.$t"},
+  //         {data: "gsx$ppg.$t"},
+  //         {data: "gsx$rushattgame.$t"},
+  //         {data: "gsx$rectarg.$t"},
+  //         {data: "gsx$recs.$t"},
+  //         {data: "gsx$totalopp.$t"},
+  //         {data: "gsx$ppa.$t"},
+  //         {data: "gsx$expectedwins.$t"},
+  //         {data: "gsx$ou.$t"},
+  //         {data: "gsx$salchange.$t"},
+  //         {data: "gsx$snapcount.$t"},
+  //         {data: "gsx$fps.$t"}
+  //         ]
+  //       });
+  //     });
+  //
+  // var spreadsheetIDk = "1RWrzWmrAZqyLlmpqNnA8kKRk9fjFN3japYiAtztRWKs";
+  // var urlk = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDk + "/od6/public/values?alt=json";
+  //
+  //   $.get(urlk , function( data ) {
+  //
+  //     //display all appointments with DATA
+  //     $('.fantasyK').DataTable( {
+  //
+  //       data: data.feed.entry,
+  //
+  //       "dom": '<"top"i>rt<"bottom"flp><"clear">',
+  //
+  //
+  //       "ordering": true,
+  //       "info":     false,
+  //       "bLengthChange": false,
+  //
+  //       columns: [
+  //         {data: "gsx$week.$t"},
+  //         {data: "gsx$player.$t"},
+  //         {data: "gsx$sal.$t"},
+  //         {data: "gsx$pos.$t"},
+  //         {data: "gsx$teamname.$t"},
+  //         {data: "gsx$injurystatus.$t"},
+  //         {data: "gsx$typeinjury.$t"},
+  //         {data: "gsx$rvp.$t"},
+  //         {data: "gsx$pr.$t"},
+  //         {data: "gsx$ppg.$t"},
+  //         {data: "gsx$totalopp.$t"},
+  //         {data: "gsx$ppk.$t"},
+  //         {data: "gsx$expectedwins.$t"},
+  //         {data: "gsx$pathexpectation.$t"},
+  //         {data: "gsx$ou.$t"},
+  //         {data: "gsx$salchange.$t"},
+  //         {data: "gsx$fps.$t"}
+  //         ]
+  //       });
+  //     });
+  //
+  // var spreadsheetIDdef = "1B63c8kUsBMC3nHhIq2vErR-55b-SgiTlhGSYIA-G2X4";
+  //
+  // var urldef = "https://spreadsheets.google.com/feeds/list/" + spreadsheetIDdef + "/od6/public/values?alt=json";
+  // $.get(urldef , function( data ) {
+  //
+  //   //display all appointments with DATA
+  //   $('.fantasyDEF').DataTable( {
+  //
+  //     data: data.feed.entry,
+  //
+  //     "dom": '<"top"i>rt<"bottom"flp><"clear">',
+  //
+  //
+  //     "ordering": true,
+  //     "info":     false,
+  //     "bLengthChange": false,
+  //
+  //     columns: [
+  //       {data: "gsx$week.$t"},
+  //       {data: "gsx$player.$t"},
+  //       {data: "gsx$sal.$t"},
+  //       {data: "gsx$pos.$t"},
+  //       {data: "gsx$teamname.$t"},
+  //       {data: "gsx$opp.$t"},
+  //       {data: "gsx$oppqbinjurystatus.$t"},
+  //       {data: "gsx$typeinjury.$t"},
+  //       {data: "gsx$opp-qbscore.$t"},
+  //       {data: "gsx$rvp.$t"},
+  //       {data: "gsx$pr.$t"},
+  //       {data: "gsx$yardsallowed.$t"},
+  //       {data: "gsx$expectedwins.$t"},
+  //       {data: "gsx$ou.$t"},
+  //       {data: "gsx$salchange.$t"},
+  //       {data: "gsx$fps.$t"}
+  //       ]
+  //     });
+  //   });
 
 
   $('.fantasynewsNBA').hide();
