9bb65e8442746802e2e559b0c9dd2e824dad211a
jeffreyyourman <jeffreyyourman@gmail.com>
2016-12-09 13:34:03 -0500

    ready to launch

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,16 @@ var mysql = require('mysql');
 
 // if (process.env.JAWSDB_URL){connection = mysql.createConnection(process.env.JAWSDB_URL)} else {connection = mysql.createConnection({host: "tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",user: 'ih88m4x1uwvaf3o2',password: 'g46smq50w2xkadp2 ',database: 'jrtc69cpagdcwlkn'});}
 
-if (process.env.JAWSDB_URL){connection = mysql.createConnection(process.env.JAWSDB_URL)} else {connection = mysql.createConnection({host: "uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",user: 'bx2ajv69tsflz7fv',password: 'bk7cnbekaw907zll',database: 'wl8zg20cpj22w77k'});}
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: 'bx2ajv69tsflz7fv',
+    password: 'bk7cnbekaw907zll',
+    database: 'wl8zg20cpj22w77k'
+  });
+}
 
 // var connection = mysql.createConnection({
 //   port: 3306,
