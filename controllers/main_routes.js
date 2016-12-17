99d1bff6dd41597e432a19087571cfb41ea53dc9
jeffreyyourman <jeffreyyourman@gmail.com>
2016-12-17 15:42:24 -0500

    ready for passport login

--- controllers/main_routes.js
+++ controllers/main_routes.js
@@ -19,7 +19,7 @@ router.get('/', function(req, res){
 
 router.get('/NFL', function (req,res){
 // "SELECT * FROM googlesheetsapiNFL where POS = 'QB' ORDER BY CritRank ASC;"
-  connection.query("SELECT * FROM googlesheetsapiNFL ORDER BY POS DESC, Sal DESC;", function(err,fantasyNFL){
+  connection.query("SELECT * FROM googlesheetsapiNFL where Player <> '' ORDER BY POS DESC, Sal DESC;", function(err,fantasyNFL){
     connection.query("SELECT * FROM googlesheetsapiNFL limit 1;", function(err,fantasyNFLDate){
     if(err) throw err;
       var hbsObject = {
@@ -33,7 +33,7 @@ router.get('/NFL', function (req,res){
 
 router.get('/NBA', function (req,res){
 
-  connection.query("SELECT * FROM googlesheetsapi ORDER BY ProjMins DESC, Salary DESC, CriteriaRanking DESC;", function(err,fantasyNBA){
+  connection.query("SELECT * FROM googlesheetsapi where Player <> '' ORDER BY ProjMins DESC, Salary DESC, CriteriaRanking DESC;", function(err,fantasyNBA){
     connection.query("SELECT * FROM googlesheetsapi limit 1;", function(err,fantasyNBADate){
     if(err) throw err;
 
