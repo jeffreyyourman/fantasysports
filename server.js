88d12b2dbe40ef9668573bc7a28039c1c93a440b
jeffreyyourman <jeffreyyourman@gmail.com>
2016-12-26 23:28:02 -0500

    change query for top plays

--- server.js
+++ server.js
@@ -124,16 +124,16 @@ app.get('/NBA', function (req,res){
 var requser = req.user;
 
 connection.query("SELECT * FROM googlesheetsapi where Player <> '' ORDER BY ProjMins DESC, Salary DESC, CriteriaRanking DESC;", function(err,fantasyNBA){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PG' AND topPlayerValue = 'Top'", function(err,fantasyNBATopPGPlay){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SG' AND topPlayerValue = 'Top'", function(err,fantasyNBATopSGPlay){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SF' AND topPlayerValue = 'Top'", function(err,fantasyNBATopSFPlay){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PF' AND topPlayerValue = 'Top'", function(err,fantasyNBATopPFPlay){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'C' AND topPlayerValue = 'Top'", function(err,fantasyNBATopCPlay){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PG' AND topPlayerValue = 'Value' ORDER BY DESC;", function(err,fantasyNBATopPGValue){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SG' AND topPlayerValue = 'Value' ORDER BY DESC;", function(err,fantasyNBATopSGValue){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SF' AND topPlayerValue = 'Value' ORDER BY DESC;", function(err,fantasyNBATopSFValue){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PF' AND topPlayerValue = 'Value' ORDER BY DESC;", function(err,fantasyNBATopPFValue){
-connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'C' AND topPlayerValue = 'Value' ORDER BY DESC;", function(err,fantasyNBATopCValue){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PG' AND topPlayerValue = 'Top' ORDER BY ProjectedPts", function(err,fantasyNBATopPGPlay){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SG' AND topPlayerValue = 'Top' ORDER BY ProjectedPts", function(err,fantasyNBATopSGPlay){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SF' AND topPlayerValue = 'Top' ORDER BY ProjectedPts", function(err,fantasyNBATopSFPlay){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PF' AND topPlayerValue = 'Top' ORDER BY ProjectedPts", function(err,fantasyNBATopPFPlay){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'C' AND topPlayerValue = 'Top' ORDER BY ProjectedPts", function(err,fantasyNBATopCPlay){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PG' AND topPlayerValue = 'Value' ORDER BY ProjectedPts DESC;", function(err,fantasyNBATopPGValue){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SG' AND topPlayerValue = 'Value' ORDER BY ProjectedPts DESC;", function(err,fantasyNBATopSGValue){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'SF' AND topPlayerValue = 'Value' ORDER BY ProjectedPts DESC;", function(err,fantasyNBATopSFValue){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'PF' AND topPlayerValue = 'Value' ORDER BY ProjectedPts DESC;", function(err,fantasyNBATopPFValue){
+connection.query("SELECT * FROM googlesheetsapiNBATopPlays where POS = 'C' AND topPlayerValue = 'Value' ORDER BY ProjectedPts DESC;", function(err,fantasyNBATopCValue){
 
 connection.query("SELECT * FROM googlesheetsapi limit 1;", function(err,fantasyNBADate){
 if(err) throw err;
