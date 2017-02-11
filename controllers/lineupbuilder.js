03a313cbc6693fb823650d3bc10fab0151ef2be2
jeffreyyourman <jeffreyyourman@gmail.com>
2017-02-10 19:11:19 -0500

    now working

--- controllers/lineupbuilder.js
+++ controllers/lineupbuilder.js
@@ -143,6 +143,11 @@ var linebuilderexport = function() {
 
 
     })
+    connection.query("delete from fanduellineup", function(err, fanduelData) {
+        if (err) throw err;
+
+        console.log("Clean fanduellineupbuilder table, ready for update");
+    })
     var lineupbuilderQuery = "select * from LINEUP ORDER BY Total_Fantasy_PointsProjected desc limit 1;";
     connection.query(lineupbuilderQuery, function(err, lineupbuilderQuery1) {
         if (err) throw err;
